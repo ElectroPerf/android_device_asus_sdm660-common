89c8ba814b2fabcfff08bb1b7bf14b67932b2cee
kubersharma001 <kubersharma001@gmail.com>
2019-07-06 16:28:16 +0530

    X01BD: Set height and width for bootanim
    Change-Id: I79871816cc6e20fe136d1234d28750d35984e444

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
