7a90a5980088a2563ceae9200d309e0b55eb4131
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 11:15:11 +0800

    sdm660-common: Switch to QTI bt stack and enable a2dp offload
    - Extract blobs from phoenix_lao_com-user 11 RKQ1.201123.002 210641533e0c8 release-keys
    
    Change-Id: I2e2bbb1db0d65fc4a4a587dc22b3a7d11a80af1e
    Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -1,8 +1,5 @@
 /*
- *
  *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
- *  Not a Contribution, Apache license notifications and license are retained
- *  for attribution purposes only.
  *
  * Copyright (C) 2012 The Android Open Source Project
  * Copyright (C) 2018 The LineageOS Project
@@ -67,7 +64,8 @@ static inline const char *BtmGetDefaultName()
 #define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
 #define GATT_MAX_PHY_CHANNEL  10
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
 #define AVDT_NUM_SEPS 35
-
 #endif
