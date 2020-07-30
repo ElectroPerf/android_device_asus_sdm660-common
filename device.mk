8ad45f829d710ade1a8f30e3c9e5e46c48150a2e
Rahul Krishna <rahulkrishna585@gmail.com>
2020-07-30 23:26:58 +0530

    X01BD: Include device overlays
    Change-Id: I8a09a9afa797899d44ef5a2f8ae442fd6289e8eb

--- device.mk
+++ device.mk
@@ -7,5 +7,10 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/asus/X01BD/X01BD-vendor.mk)
