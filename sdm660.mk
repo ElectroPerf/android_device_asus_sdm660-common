da6b006bf6ae8c1931ec950912f98082789859c2
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 13:09:27 +0530

    sdm660-common: Include power-libperfmgr Dependencies
    - hardware/google/interfaces and hardware/google/pixel
    
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -423,7 +423,9 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
