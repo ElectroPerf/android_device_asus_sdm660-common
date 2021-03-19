ed30fa1c521de92ade5ffe902ec2fd5581461e12
etahamad <etahamad@icloud.com>
2021-03-19 08:59:18 +0200

    sdm660-common: Introduce IORap
    Commit b73d5516136d1a4de39afd74f564daf70b946889 in build/make (rvc-qpr2)
    disabled IORap by default due to a "regression" without mentioning what
    the regression is. Considering how it's been working fine for the past 6
    months and has demonstrated improvements to app launch time [1], let's
    re-enable it here.
    
    [1] https://medium.com/androiddevelopers/improving-app-startup-with-i-o-prefetching-62fbdb9c9020
    
    Co-authored-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
    Co-authored-by: DillerOFire <niktofe1@gmail.com>
    Co-authored-by: Nikhil Baddi <nikhilbaddi98@gmail.com>
    Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -203,6 +203,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Iorap
+PRODUCT_PACKAGES += \
+    iorap-app-startup-runner
+
 # IPC router config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
