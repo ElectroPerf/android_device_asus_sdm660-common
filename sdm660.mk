a0beecebd56da26e955c363171dc25c77c8c15d4
SakilMondal <mondalsakil@gmail.com>
2021-05-02 21:52:23 +0530

    sdm660-common: Drop non-functional soundtrigger
    * Fixes google assistant voice match in android 11
    
    Change-Id: I0fb85732fd1abfd4f525bf4b76a89f85afd11375

--- sdm660.mk
+++ sdm660.mk
@@ -25,8 +25,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.audio.effect@2.0-service
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
