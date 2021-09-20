172a525ff893154967a827b1de5264be6c8f6a8a
Kunmun <kunmun.devroms@gmail.com>
2021-09-20 15:25:24 +0000

    sdm660-common: BoardConfig: Declare Target Clang As Proton
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,7 @@ BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/asus/sdm660
 TARGET_KERNEL_VERSION := 4.19
+TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
