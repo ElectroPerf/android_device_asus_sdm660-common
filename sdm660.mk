5bcbcc8da0df3e3dfebe0b87e051d7252731da7e
SebaUbuntu <SebaUbuntu@users.noreply.github.com>
2021-09-25 17:51:39 +0530

    sdm660-common: lights: Pull in AIDL Hal from `sm8250-common`
    • Drop white and blue led support
    • Adapt for Asus SDM660 msm-4.19
    • Grant permissions to nodes properly
    
    Change-Id: I118348b128741601ac0827c8ead7686d398f0d64
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -220,7 +220,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.asus_sdm660
+    android.hardware.lights-service.asus_sdm660
 
 # Media
 PRODUCT_PACKAGES += \
