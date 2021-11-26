2fefa2a3a7e0e7c23b7be2f6565def9d2a540f2f
ElectroPerf <kunmun.devroms@gmail.com>
2021-11-26 15:45:15 +0530

    sdm660-common: Drop `File Based Encryption(FBE)`
    • Its broken with AOSP's keymaster 4.x
    • We're better off with Disable Force Ecryption(DFE) for time being till we manage fixing it
    
    Change-Id: Ieed6210c03f40a95b35d92ea579b6c687cfa37ea
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -187,6 +187,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
@@ -244,10 +248,6 @@ PRODUCT_PACKAGES += \
     NfcNci \
     SecureElement \
     Tag
-
-# OEM Unlock reporting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
 
 # OMX
 PRODUCT_PACKAGES += \
@@ -411,11 +411,6 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
-
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
