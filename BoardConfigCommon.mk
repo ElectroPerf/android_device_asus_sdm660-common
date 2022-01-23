ccdc153af11eb183632ecbbac85d4fb7ccc6cce7
Kunmun <kunmun.devroms@gmail.com>
2022-01-24 02:13:57 +0530

    sdm660-common: vintf: Move all related XMLs into `/configs` directory
    Change-Id: Iff9d10314f156b07045e342c39a4e3ea42970435
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,15 +88,15 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 LOC_HIDL_VERSION := 4.0
 
 # HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/device_framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/configs/vintf/device_framework_compatibility_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += NFC
-ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
+ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/configs/vintf/manifest_nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sdm660
