78525c5592923172ed981a4a0383139891c63823
xboxfanj <jake@aospa.co>
2022-01-24 14:46:54 +0530

    sdm660-common: configs: filesystem: Move to `vendor/aospa`
    Change-Id: I95f01d70a25856401238681a1431d835820b1f70
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,9 +61,6 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
-
 # FM
 BOARD_HAS_QCA_FM_SOC := cherokee
 BOARD_HAVE_QCOM_FM := true
