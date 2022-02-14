a07a0f4e51f95859a0e1c81a3360e606d9a54119
Kunmun <kunmun.devroms@gmail.com>
2022-02-15 01:27:30 +0530

    sdm660-common: seccomp: Move to QTI common components
    Change-Id: I2ec14b6463ca51b2be6a16cf5619f7ff61926356

--- sdm660.mk
+++ sdm660.mk
@@ -459,10 +459,6 @@ PRODUCT_PACKAGES += \
     init.qcom.asus.rc \
     init.asus.biometrics.rc
 
-# Seccomp
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
