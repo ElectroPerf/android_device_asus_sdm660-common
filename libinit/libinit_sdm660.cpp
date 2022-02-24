d6f6f57841c4ac0270c65a887e998177c2aaa46b
Kunmun <kunmun.devroms@gmail.com>
2022-02-24 20:24:28 +0530

    sdm660-common: audio: Enable and override Compress VoIP
    Change-Id: I718c092f90684877cba93823795b55aa32f770ef

--- libinit/libinit_sdm660.cpp
+++ libinit/libinit_sdm660.cpp
@@ -94,4 +94,5 @@ void vendor_load_properties()
     property_override("persist.vendor.audio.fluence.voicerec", "true");
     property_override("persist.vendor.audio.fluence.speaker", "false");
     property_override("ro.vendor.audio.sdk.fluencetype", "fluence");
+    property_override("vendor.audio.feature.compr_voip.enable", "true");
 }
