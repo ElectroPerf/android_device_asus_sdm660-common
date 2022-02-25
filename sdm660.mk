952df12f9d1a2cfee9d1f8edb89d8d6fb22291b3
Marijn Suijten <marijns95@gmail.com>
2022-02-25 18:57:09 +0530

    sdm660-common: Makefile: Build (unused) qti mapper@2.0 interface libs
    The blobs link against this interface library because of a fallback
    mechanism when mapper@3.0 isn't made available (all our devices should
    always provide that, never 2.0). Consequently this results in a silent
    failure (not even a linker error appears) when trying to open the GPU
    through EGL:
    
        I AdrenoGLES-0: Driver Path                      : /odm/lib64/egl/libGLESv2_adreno.so
        W libEGL  : eglInitialize(0xb40000743497a2d0) failed (EGL_BAD_ALLOC)
        E libEGL  : call to OpenGL ES API with no current context (logged once per thread)
        F RenderEngine: eglQueryStringImplementationANDROID(EGL_VERSION) failed
    
    Adding this library - which is not explicitly linked/used by any other
    bits in the OSS part of the stack - resolves that error.
    
    Note that while the composer has a similar fallback mechanism to 2.0 it
    only uses (and links against) the AOSP interface
    (`android.hardware.graphics.mapper@2.0`), not the QTI one at
    `vendor.qti.hardware.display.mapper@2.0`.
    
    Note^2: All these hidl interfaces are defined as `system_ext`, which is
    the desired place for packages (in this case interface
    updates/extensions to the AOSP variant) may they ever land upstream.
    We have not configured such a partition currently (on super) but instead
    rely on it being in `/system/system_ext`. This is not compatible with
    GSIs; we just request the vendor variant to be put on `/vendor`
    (`.vendor` postfix on the package name).
    
    Change-Id: Iee91979ca5f10ddac61e0b4389be7ee16f3a8888

--- sdm660.mk
+++ sdm660.mk
@@ -144,7 +144,8 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.allocator-service
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.hwc.mdpcomp.enable=true \
