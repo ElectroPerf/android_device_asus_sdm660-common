7fcb62206eaa49983df57804091ce8067bf8707a
Kunmun <kunmun.devroms@gmail.com>
2022-03-29 10:26:33 +0530

    sdm660-common: BoardConfig: Drop duplicate soong namespace
    • Already present in device makefile
    
    Change-Id: I757dda40b9c1c36b76c31c82ffefa029d3466310

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,9 +134,6 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
-
 # Treble
 BOARD_VNDK_VERSION := current
 
