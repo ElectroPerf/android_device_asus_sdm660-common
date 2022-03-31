59cc17df476e18ab1f3006860a3f8e84171cd0da
intervigilium <intervigil@gmail.com>
2022-03-31 09:13:47 +0530

    sdm660-common: Use soong namespace to avoid build conficts
    Change-Id: Ib9037d2d8d082ef786fc461cfd7db0383a281d93

--- sdm660.mk
+++ sdm660.mk
@@ -7,5 +7,8 @@
 # Board Platform
 TARGET_BOARD_PLATFORM := sdm660
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/asus/sdm660-common/sdm660-common-vendor.mk)
