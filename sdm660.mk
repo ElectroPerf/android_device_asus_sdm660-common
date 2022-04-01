846749e02b57b8283a4f1ba7b2666eb000745e4d
Kunmun <kunmun.devroms@gmail.com>
2022-04-01 12:03:16 +0530

    sdm660-common: media: Import configs and props from LA.UM.10.2.1.r1-03400-sdm660.0
    • Build required media packages
    • Inherit QTI medi-legacy component
    
    Change-Id: Id1828f9fad2c939b8d87f0cc2562a32b2aa0026e

--- sdm660.mk
+++ sdm660.mk
@@ -11,6 +11,17 @@ PRODUCT_PACKAGES += \
 # Board Platform
 TARGET_BOARD_PLATFORM := sdm660
 
+# Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec \
+    android.hardware.media.c2@1.0.vendor \
+    android.hardware.media.c2@1.1.vendor
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0
+
 # Input
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
@@ -22,6 +33,34 @@ PRODUCT_COPY_FILES += \
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
 
+# Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    mediametrics \
+    libmediametrics \
+    libregistermsext \
+    libstagefright_enc_common \
+    libavmediaserviceextensions
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    vidc.enc.target_support_bframe=1 \
+    vendor.vidc.enc.disable_bframes=1 \
+    vendor.vidc.dec.enable.downscalar=1 \
+    vendor.vidc.enc.disable.pq=true
+
+# OMX
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw \
+    libmm-omxcore \
+    libstagefrighthw \
+    libc2dcolorconvert
+
 # Overlay
 PRODUCT_PACKAGES += \
     AsusSDM660Frameworks \
@@ -32,6 +71,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # QTI Common Components
+QTI_COMPONENTS += media-legacy
 QTI_COMPONENTS += overlay
 TARGET_COMMON_QTI_COMPONENTS += $(QTI_COMPONENTS)
 
